8e18e4ba77120a9e87f4de7558e8b7b38adc22cb
blobby-bobby <bulle.ouvrard@gmail.com>
2024-02-15 16:39:26 +0100

    spline change on nighttime

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -8,9 +8,25 @@ export interface WeatherData {
       country: string;
     }
     weather: {
+      main: string;
       description: string;
     }[];
     wind: {
       speed: number;
     }
   }
+
+interface SplineData {
+  main: string, 
+  urlId: string,
+}
+
+export const SplineSelectionDay: SplineData[] = [
+    {main: "Thunderstorm", urlId: ""},
+    {main: "Drizzle", urlId: ""},
+    {main: "Rain", urlId: ""},
+    {main: "Snow", urlId: ""},
+    {main: "Atmosphere", urlId: ""},
+    {main: "Clear", urlId: ""},
+    {main: "Clouds", urlId: ""},
+]
